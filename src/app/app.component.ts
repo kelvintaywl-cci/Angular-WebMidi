889cad15b25e104cb28c036c066ca437ef53a044
VV <vv@vv.local>
2022-09-14 21:47:36 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  status:string = 'checking'
+
   title = 'Angular-WebMidi';
+
+  midiAccess!:WebMidi.MIDIAccess;
+
+  constructor () {
+
+    this.initMIDI();
+
+  }
+
+  initMIDI = () => {
+  
+    if ( navigator.requestMIDIAccess ) {
+    
+      navigator.requestMIDIAccess( {sysex: true, software:true } as any )
+      .then( this.onMIDISuccess, this.onMIDIFailure );
+    
+    } else {
+    
+      this.status = 'WebMidi is not supported';
+    
+    }
+  
+  }
+
+  onMIDISuccess = ( midiAccess:WebMidi.MIDIAccess ) => {
+
+    this.midiAccess = midiAccess;
+
+    this.status = 'success';
+
+  }
+
+  onMIDIFailure = ( e:any ) => this.status = 'failed';
+
 }
